1274593d711c8a459b57496d54f2b13bc1e6d687
Eduardo Mathias de Souza <eduardo.souza@ort.org.br>
2018-07-04 17:59:52 +0000

    Adiciona os modelos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
-  get 'static_pages/hi'
-
- root 'application#hello'
+rails server
+  get  'static_pages/home'
+  get  'static_pages/hi'
+  root 'application#hello'
+  get 'static_pages/home'
 end
