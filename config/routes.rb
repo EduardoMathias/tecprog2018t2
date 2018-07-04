38339aacbd928efbfff82159f991ea0ce9451c1a
Eduardo Mathias de Souza <eduardo.souza@ort.org.br>
2018-07-04 18:50:20 +0000

    Resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get  'static_pages/hi'
   root 'application#hello'
+  root 'user#index'
+  root 'picture#index'
+  root 'recipient#index'
 end
